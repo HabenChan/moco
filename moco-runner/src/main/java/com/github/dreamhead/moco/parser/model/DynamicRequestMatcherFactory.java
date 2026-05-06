affe642c9d30f1cf2456ab7acb7f4009a80fc7b8
Zheng Ye <dreamhead.cn@gmail.com>
2026-05-06 11:25:08 +0800

    applied switch expression in dynamic request matcher factory

--- moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/DynamicRequestMatcherFactory.java
+++ moco-runner/src/main/java/com/github/dreamhead/moco/parser/model/DynamicRequestMatcherFactory.java
@@ -190,13 +190,10 @@ public final class DynamicRequestMatcherFactory extends Dynamics implements Requ
 
     private static RequestMatcher wrapRequestMatcher(final RequestSetting request,
                                                      final List<RequestMatcher> matchers) {
-        switch (matchers.size()) {
-            case 0:
-                throw new IllegalArgumentException("illegal request setting:" + request);
-            case 1:
-                return matchers.get(0);
-            default:
-                return new AndRequestMatcher(matchers);
-        }
+        return switch (matchers.size()) {
+            case 0 -> throw new IllegalArgumentException("illegal request setting:" + request);
+            case 1 -> matchers.get(0);
+            default -> new AndRequestMatcher(matchers);
+        };
     }
 }
