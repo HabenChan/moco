589ad0b0b5f5ac543b2f1e66b1efe98a3be882a5
Zheng Ye <dreamhead.cn@gmail.com>
2026-05-09 16:56:49 +0800

    modernized gradle script

--- moco-runner/src/test/java/com/github/dreamhead/moco/MocoCookieStandaloneTest.java
+++ moco-runner/src/test/java/com/github/dreamhead/moco/MocoCookieStandaloneTest.java
@@ -1,13 +1,11 @@
 package com.github.dreamhead.moco;
 
 import com.google.common.net.HttpHeaders;
-import io.netty.handler.codec.http.cookie.ClientCookieDecoder;
-import io.netty.handler.codec.http.cookie.Cookie;
-import io.netty.handler.codec.http.cookie.CookieHeaderNames;
-import io.netty.handler.codec.http.cookie.DefaultCookie;
+import org.apache.hc.core5.http.ClassicHttpResponse;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.net.HttpCookie;
 
 import static com.github.dreamhead.moco.helper.RemoteTestUtils.remoteUrl;
 import static org.hamcrest.CoreMatchers.is;
@@ -17,69 +15,72 @@ public class MocoCookieStandaloneTest extends AbstractMocoStandaloneTest {
     @Test
     public void should_set_and_recognize_cookie() throws IOException {
         runWithConfiguration("cookie.json");
-        Cookie decodeCookie = getCookie("/cookie");
-        assertThat(decodeCookie.name(), is("login"));
-        assertThat(decodeCookie.value(), is("true"));
+        HttpCookie decodeCookie = getCookie("/cookie");
+        assertThat(decodeCookie.getName(), is("login"));
+        assertThat(decodeCookie.getValue(), is("true"));
     }
 
     @Test
     public void should_set_and_recognize_cookie_with_path() throws IOException {
         runWithConfiguration("cookie.json");
-        Cookie decodeCookie = getCookie("/cookie-with-path");
-        assertThat(decodeCookie.name(), is("login"));
-        assertThat(decodeCookie.value(), is("true"));
-        assertThat(decodeCookie.path(), is("/"));
+        HttpCookie decodeCookie = getCookie("/cookie-with-path");
+        assertThat(decodeCookie.getName(), is("login"));
+        assertThat(decodeCookie.getValue(), is("true"));
+        assertThat(decodeCookie.getPath(), is("/"));
     }
 
     @Test
     public void should_set_and_recognize_cookie_with_domain() throws IOException {
         runWithConfiguration("cookie.json");
-        Cookie decodeCookie = getCookie("/cookie-with-domain");
-        assertThat(decodeCookie.name(), is("login"));
-        assertThat(decodeCookie.value(), is("true"));
-        assertThat(decodeCookie.domain(), is("github.com"));
+        HttpCookie decodeCookie = getCookie("/cookie-with-domain");
+        assertThat(decodeCookie.getName(), is("login"));
+        assertThat(decodeCookie.getValue(), is("true"));
+        assertThat(decodeCookie.getDomain(), is("github.com"));
     }
 
     @Test
     public void should_set_and_recognize_cookie_with_secure() throws IOException {
         runWithConfiguration("cookie.json");
-        Cookie decodeCookie = getCookie("/cookie-with-secure");
-        assertThat(decodeCookie.name(), is("login"));
-        assertThat(decodeCookie.value(), is("true"));
-        assertThat(decodeCookie.isSecure(), is(true));
+        HttpCookie decodeCookie = getCookie("/cookie-with-secure");
+        assertThat(decodeCookie.getName(), is("login"));
+        assertThat(decodeCookie.getValue(), is("true"));
+        assertThat(decodeCookie.getSecure(), is(true));
     }
 
     @Test
     public void should_set_and_recognize_cookie_with_http_only() throws IOException {
         runWithConfiguration("cookie.json");
-        Cookie decodeCookie = getCookie("/cookie-with-http-only");
-        assertThat(decodeCookie.name(), is("login"));
-        assertThat(decodeCookie.value(), is("true"));
+        HttpCookie decodeCookie = getCookie("/cookie-with-http-only");
+        assertThat(decodeCookie.getName(), is("login"));
+        assertThat(decodeCookie.getValue(), is("true"));
         assertThat(decodeCookie.isHttpOnly(), is(true));
     }
 
     @Test
     public void should_set_and_recognize_cookie_with_max_age() throws IOException {
         runWithConfiguration("cookie.json");
-        Cookie decodeCookie = getCookie("/cookie-with-max-age");
-        assertThat(decodeCookie.name(), is("login"));
-        assertThat(decodeCookie.value(), is("true"));
-        assertThat(decodeCookie.maxAge(), is(3600L));
+        HttpCookie decodeCookie = getCookie("/cookie-with-max-age");
+        assertThat(decodeCookie.getName(), is("login"));
+        assertThat(decodeCookie.getValue(), is("true"));
+        assertThat(decodeCookie.getMaxAge(), is(3600L));
     }
 
     @Test
     public void should_set_and_recognize_cookie_with_same_site() throws IOException {
         runWithConfiguration("cookie.json");
-        DefaultCookie decodeCookie = getCookie("/cookie-with-same-site");
-        assertThat(decodeCookie.name(), is("login"));
-        assertThat(decodeCookie.value(), is("true"));
-        assertThat(decodeCookie.sameSite(), is(CookieHeaderNames.SameSite.Lax));
+        String value = getCookieHeader("/cookie-with-same-site");
+        assertThat(value.contains("login=true"), is(true));
+        assertThat(value.contains("SameSite=Lax"), is(true));
     }
 
-    private DefaultCookie getCookie(String uri) throws IOException {
-        org.apache.hc.core5.http.HttpResponse response = helper.getResponse(remoteUrl(uri));
-
+    private HttpCookie getCookie(String uri) throws IOException {
+        ClassicHttpResponse response = helper.getResponse(remoteUrl(uri));
         String value = response.getFirstHeader(HttpHeaders.SET_COOKIE).getValue();
-        return (DefaultCookie)ClientCookieDecoder.STRICT.decode(value);
+        return HttpCookie.parse(value).get(0);
+    }
+
+    private String getCookieHeader(String uri) throws IOException {
+        ClassicHttpResponse response = helper.getResponse(remoteUrl(uri));
+        return response.getFirstHeader(HttpHeaders.SET_COOKIE).getValue();
     }
 }
